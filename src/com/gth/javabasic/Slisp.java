2f8c7ae188e494141b33f7d97835f036cdadb76a
Giltaehyeong <r1605866@naver.com>
2022-05-12 17:31:38 +0900

    Ch01. CommitTest

---
+++ src/com/gth/javabasic/Slisp.java
@@ -0,0 +1,9 @@
+package com.gth.javabasic;
+
+public class Slisp {
+
+	public static void main(String[] args) {
+		System.out.println("프로그램 실습 test 메세지 입니다.");
+		System.out.println("깃 업로드 확인.");
+	}
+}
