01c61c3e760cf0a06895a0faef0c20afe2409321
r1605 <r1605@192.168.124.101>
2022-05-14 01:25:12 +0900

    Ch02. 변수(Variable) / VarEx2.java * 실습목표 - 빈 컵을 이용해서 두 컵의 내용물을 바꾸는 일을 직접 해보자. - 상수를 사용하여 삼각형과 사각형의 면적을 구하는 프로그램을 만들어보자.

--- src/com/gth/javabasic/Slisp.java
+++ src/com/gth/javabasic/Slisp.java
@@ -3,20 +3,32 @@ package com.gth.javabasic;
 public class Slisp {
 
 	public static void main(String[] args) {
-		int year = 0;
-		int age = 14;
+		int a = 1;
+		int b = 13;
+		int temp = 0;
 		
-		System.out.println(year);
-		System.out.println(age);
+		System.out.println(a);
+		System.out.println(b);
 		
-		year = age + 2000; // 변수 age의 값에 2000을 더해서 변수 year에 저장
+		temp = a;
+		a = b;
+		b = temp;
 		
-		System.out.println(year);
+		System.out.println(a);
+		System.out.println(b);
 		
-		age = age + 1; // 변수 age에 저장된 값을 1증가시킨다.
+		final int WIDTH = 20; //폭
+		final int HEIGHT = 10; //높이
 		
-		System.out.println(age);
+		//상수 사용예제
+		int triangleArea = (WIDTH * HEIGHT) / 2; // 삼각형의 면적을 구하는 공식
+		int rectangleArea = WIDTH * HEIGHT; // 사각형의 면적을 구하는 공식
+		
+		System.out.println(triangleArea);
+		System.out.println(rectangleArea);
+		
+		char ch = 'J'; //문자 리터널
+		String name = "Java"; // 변수 name에 문자열 리터널 "Java"를 저장
 	}
-
 }
 
