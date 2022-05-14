f1105b073f21875dc9912afab38add4433cd19ec
Giltaehyeong <r1605866@naver.com>
2022-05-14 21:48:40 +0900

    Ch02. 변수(Variable) / StringEx.java * 실습목표 - 문자열과 덧셈연산을 수행하면 그 결과는 문자열이다. 결과를 통해 확인. - 7 + " " = "7 " - 7 + "7" = "77"

--- src/com/gth/javabasic/Slisp.java
+++ src/com/gth/javabasic/Slisp.java
@@ -1,34 +1,18 @@
 package com.gth.javabasic;
 
 public class Slisp {
-
 	public static void main(String[] args) {
-		int a = 1;
-		int b = 13;
-		int temp = 0;
+		String name = "Ja" + "va";
+		String str = name + 8.0;
 		
-		System.out.println(a);
-		System.out.println(b);
-		
-		temp = a;
-		a = b;
-		b = temp;
-		
-		System.out.println(a);
-		System.out.println(b);
-		
-		final int WIDTH = 20; //폭
-		final int HEIGHT = 10; //높이
-		
-		//상수 사용예제
-		int triangleArea = (WIDTH * HEIGHT) / 2; // 삼각형의 면적을 구하는 공식
-		int rectangleArea = WIDTH * HEIGHT; // 사각형의 면적을 구하는 공식
-		
-		System.out.println(triangleArea);
-		System.out.println(rectangleArea);
-		
-		char ch = 'J'; //문자 리터널
-		String name = "Java"; // 변수 name에 문자열 리터널 "Java"를 저장
+		System.out.println(name);
+		System.out.println(str);
+		System.out.println(7 + " ");
+		System.out.println(" " + 7);
+		System.out.println(7 + "");
+		System.out.println("" + 7);
+		System.out.println(7 + 7 + "");
+		System.out.println("" + 7 + 7);
 	}
 }
 
