39bfa8b796517a5c9d306c2a57cfd58463681fc8
Giltaehyeong <r1605866@naver.com>
2022-05-17 10:04:29 +0900

    Ch02. 변수(Variable) / ScannerEx.java * 실습목표 - 화면에서 입력받기 - Scanner - import java.util.*; scanner클래스를 사용하기 위해 추가 - Scanner scanner = new Scanner(System.in); //Scanner 객체를 생성 - print("메세지"); - String input = scanner.nextLine(); // 입력받은 내용을 input에 저장 - int num = Integer.parseInt(input); // 입력받은 내용을 int타입의 값으로 변환 - 입력받은 문자열을 숫자로 변환하려면, Integer.parseInt()라는 메서드를 이용한다. - 숫자가 아닌 문자 또는 기호를 입력하면, 입력받은 문자열을 숫자로 변환하는 과정인 Integer.parseInt()에서 오류남 - 공백을 입력하지 않도록 주의하자.

--- src/com/gth/javabasic/Slisp.java
+++ src/com/gth/javabasic/Slisp.java
@@ -1,27 +1,17 @@
 package com.gth.javabasic;
 
+import java.util.Scanner;
+
 public class Slisp {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in); //Scanner 객체를 생성
 		
-		String url = "www.codechobo.com";
+		System.out.print("두자리 정수를 하나 입력해주세요>");
+		String input = scanner.nextLine(); // 입력받은 내용을 input에 저장
+		int num = Integer.parseInt(input); // 입력받은 내용을 int타입의 값으로 변환
 		
-		float f1 = .10f; //0.10, 1.0e-1
-		float f2 =1e1f; // 10.0, 1.0e1, 1.0e+1
-		float f3 = 3.14e3f;
-		double d = 1.23456789;
-		
-		System.out.printf("f1=%f, %e, %g%n", f1, f1, f1);
-		System.out.printf("f2=%f, %e, %g%n", f2, f2, f2);
-		System.out.printf("f3=%f, %e, %g%n", f3, f3, f3);
-		
-		System.out.printf("d=%f%n", d); //마지막 자리 반올림됨.
-		System.out.printf("d=%14.10f%n", d); // 전체자리.소수점아래자리f
-		
-		System.out.printf("[12345678901234567890]%n");
-		System.out.printf("[%s]%n", url); // 문자열의 길이만큼 출력공간을 확보
-		System.out.printf("[%20s]%n", url); // 최소 20글자 출력공간 확보.(우측정렬)
-		System.out.printf("[%-20s]%n", url); // 최소 20글자 출력공간 확보.(좌측정렬)
-		System.out.printf("[%.8s]%n", url); // 왼쪽에서 8글자만 출력
+		System.out.println("입력내용 : "+ input);
+		System.out.printf("num = %d%n", num);
 	}
 }
 
