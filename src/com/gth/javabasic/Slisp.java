c54d6318cdf564901d0fd6148267cbe0812650ca
Giltaehyeong <r1605866@naver.com>
2022-05-24 11:31:49 +0900

    Ch06. 객체지향 프로그래밍(1) / TvTest3.java  - t1, t2 모두 같은 Tv클래스의 인스턴스를 가리키고 있기 때문에 t1.channel과 t2.channel의 값은 7이며 같은 결과가 화면에 출력된다.  - 참조변수에는 하나의 값(주소)만이 저장될 수 있으므로 둘 이상의 참조변수가 하나의 인스턴스를 가르키는(참조하는) 것은 가능하지만 하나의 참조변수로 여러 개의 인스턴스를 가리키는 것은 가능하지 않다.

--- src/com/gth/javabasic/Slisp.java
+++ src/com/gth/javabasic/Slisp.java
@@ -13,12 +13,13 @@ class Tv{ ; //1. 클래스생성(설계도)
 
 public class Slisp {
 	public static void main(String[] args) {
-		Tv t1 = new Tv(); //Tv t1; t1 = new Tv();를 한문장으로 가능
+		Tv t1 = new Tv(); 
 		Tv t2 = new Tv();
 		System.out.println("현재 채널은 " + t1.channel + "입니다.");
 		System.out.println("현재 채널은 " + t2.channel + "입니다.");
 		
-		t1.channel = 7; //channel의 값을 7으로 한다.
+		t2 = t1; // t1이 저장하고  있는 값(주소)을 t2에 저장한다.
+		t1.channel = 7; // channel 값을 7로 한다.
 		System.out.println("t1의 channel값을 7로 변경하였습니다.");
 		
 		System.out.println("t1의 channel 값은 " + t1.channel + "입니다.");
