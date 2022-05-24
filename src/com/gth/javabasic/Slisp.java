3a02170ea2fb1c0671ec20902a242210c0a9464c
Giltaehyeong <r1605866@naver.com>
2022-05-24 11:04:31 +0900

    ch6. TvTest.java  - Tv클래스로부터 인스턴스를 생성하고 인스턴스의 속성(channel)과 메서드(channelDown())를 사용하는 방법을 보여주는 실습

--- src/com/gth/javabasic/Slisp.java
+++ src/com/gth/javabasic/Slisp.java
@@ -1,21 +1,25 @@
 package com.gth.javabasic;
 
+class Tv{ ; //1. 클래스생성(설계도)
+	String color; // 색상
+	boolean power; // 전원상태(on/off)
+	int channel; // 채널
+
+	// 2. 객체 생성(제품의 기능을 생성한다.)
+	void power() { power = !power; } // TV를 켜거나 끄는 기능을 하는 메서드
+	void vhannelUp() { ++channel; } // TV의 채널을 높이는 기능을 하는 메서드
+	void channelDown() { --channel; } // TV의 채널을 낮추는 기능을 하는 메서드
+	
+}
+
 public class Slisp {
 	public static void main(String[] args) {
-		int i = 91234567; // 8자리의 10진수
-		float f = (float)i; // int를 float로 형변환
-		int i2 = (int)f; // float를 다시 int로 형변환
-		
-		double d = (double)i; // int를 double로 형변환
-		int i3 = (int)d; // double을 다시 int로 형변환
-	
-		float f2 = 1.666f;
-		int i4 = (int)f2;
-		
-		System.out.printf("i=%d%n", i);
-		System.out.printf("f=%f i2=%d%n", f, i2);
-		System.out.printf("d=%f i3=%d%n", d, i3);
-		System.out.printf("(int) %f=%d%n", f2, i4);
+		Tv t;  // Tv인스턴스를 참조하기 위한 변수, t를 선언
+		t = new Tv(); // Tv 인스턴스를 생성한다.
+		t.channel = 7; // Tv 인스턴스의 멤버변수 channel의 값을 7로한다.
+		// 3. 객체 사용(제품의 기능을 사용한다.)
+		t.channelDown(); // Tv 인스턴스의 메서드 channelDown을 호출한다.
+		System.out.println("현재 채널은 " + t.channel + "입니다.");
 	}
 }
 
