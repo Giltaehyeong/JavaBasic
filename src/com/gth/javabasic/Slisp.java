f65e43e5e4143c534a0339f0ee34616ef95c0159
seulgae <r1605866@naver.com>
2022-05-30 16:10:46 +0900

    Ch06. 객체지향 프로그래밍(1) / ReferenceParamEx1-1.java  - 참조형 반환타입

--- src/com/gth/javabasic/Slisp.java
+++ src/com/gth/javabasic/Slisp.java
@@ -6,8 +6,10 @@ public class Slisp { // 프로그램 실행
 	public static void main(String[] args) {
 		Data3 d = new Data3(); 
 		d.x = 10;
+		System.out.println(d);
 		
 		Data3 d2 = copy(d);
+		System.out.println(d2);
 		System.out.println("d.x = " + d.x);
 		System.out.println("d2.x =  " + d2.x);
 	} 
@@ -15,6 +17,7 @@ public class Slisp { // 프로그램 실행
 	static Data3 copy(Data3 d) { // 메서드의 반환타입이 참조형이라는 것은 복사한 객체의 주소를 반환한다는 의미로 해석하자.
 		Data3 tmp = new Data3(); // 새로운 객체 tmp를 생성한다.
 		tmp.x = d.x;  // d.x 의 값을 tmp.x 에 복사한다.
+		System.out.println(tmp);
 		return tmp; // 복사한 객체의 주소를 반환한다.
 	} 
 }
