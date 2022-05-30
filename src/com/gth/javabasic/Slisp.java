12c7794d6b0f4a6c2c81b5b2eba15e7190d814f7
seulgae <r1605866@naver.com>
2022-05-30 13:55:52 +0900

    Ch06. 객체지향 프로그래밍(1) / MyMathTest.java  - 사칙연산을 위한 4개의 메서드가 정의되어 있는 MyMath 클래스를 이용한 예제  + 두 값을 받아서 둘중에 큰 값을 반환하는 메서드를 작성하시오.  + printGugudan 구구단 출력 프로그램, return 사용법  + return문 조건식 프로그램

--- src/com/gth/javabasic/Slisp.java
+++ src/com/gth/javabasic/Slisp.java
@@ -8,16 +8,27 @@ public class Slisp {
 		long result2 = mm.subtract(5L, 3L);
 		long result3 = mm.multiply(5L, 3L);
 		double result4 = mm.divide(5L, 3L);
+		mm.printGugudan(0);
 		
 		System.out.println("max(5L, 3L) = " + result);
 		System.out.println("add(5L, 3L) = " + result1);
 		System.out.println("subtract(5L, 3L) = " + result2);
 		System.out.println("multiply(5L, 3L) = " + result3);
 		System.out.println("divide(5L, 3L) = " + result4);
+		
 	}
 }
 
 class MyMath {
+	void printGugudan(int dan) {
+		if(!(2<=dan && dan<=9))
+			return; // 입력받은 단(dan)이 2~9가 아니면, 메서드 종료하고 돌아가기
+		
+		for(int i=1; i<=9; i++) {
+			System.out.printf("%d * %d = %d%n", dan, i, dan * i);
+		}
+	}
+	
 	long add(long a, long b) {
 		long result = a+b;
 		return result;
@@ -25,7 +36,11 @@ class MyMath {
 	}
 	// 두 값을 받아서 둘중에 큰 값을 반환하는 메서드를 작성하시오.
 	long max(long a, long b) {
-		return a > b ? a : b;
+		if(a>b)
+			//This method must return a result of type long
+			return a; // 조건식이 참일떄만 실행
+		else
+			return b; // 조건식이 거짓일때 실행
 	}
 	long subtract(long a, long b) { return a-b;}
 	long multiply(long a, long b) {return a*b;}
