d68f0225b6959e825dc50e51f8bb37231527cd03
seulgae <r1605866@naver.com>
2022-05-30 14:52:42 +0900

    Ch06. 객체지향 프로그래밍(1) / PrimitiveParamEx.java  - 기본형 매개변수 호출 스택

--- src/com/gth/javabasic/Slisp.java
+++ src/com/gth/javabasic/Slisp.java
@@ -1,50 +1,20 @@
 package com.gth.javabasic;
 
-public class Slisp {
-	public static void main(String[] args) {
-		MyMath mm = new MyMath();
-		long result = mm.max(5L, 3L); // 둘 중에 큰 값을 받환하는 메서드
-		long result1 = mm.add(5L, 3L);
-		long result2 = mm.subtract(5L, 3L);
-		long result3 = mm.multiply(5L, 3L);
-		double result4 = mm.divide(5L, 3L);
-		mm.printGugudan(0);
-		
-		System.out.println("max(5L, 3L) = " + result);
-		System.out.println("add(5L, 3L) = " + result1);
-		System.out.println("subtract(5L, 3L) = " + result2);
-		System.out.println("multiply(5L, 3L) = " + result3);
-		System.out.println("divide(5L, 3L) = " + result4);
-		
-	}
-}
+class Data { int x; }
 
-class MyMath {
-	void printGugudan(int dan) {
-		if(!(2<=dan && dan<=9))
-			return; // 입력받은 단(dan)이 2~9가 아니면, 메서드 종료하고 돌아가기
+public class Slisp { // 프로그램 실행
+	public static void main(String[] args) { //1. main 메서드 실행 In
+		Data d = new Data(); // 객체 생성
+		d.x = 10; // 객체 참조변수 x변수에 10대입
+		System.out.println("main() : x= " + d.x); //2. println 메서드 실행 In , 3. 콘솔 출력시 out
 		
-		for(int i=1; i<=9; i++) {
-			System.out.printf("%d * %d = %d%n", dan, i, dan * i);
-		}
-	}
+		change(d.x); //chagne 메서드 호출, 호출한곳으로 되돌아옴.
+		System.out.println("After change(d.x)"); //8. println 메서드 실행 In , 3. 콘솔 출력시 out
+		System.out.println("main() : x = "+ d.x); //9.  println 메서드 실행 In , 3. 콘솔 출력시 out
+	} // 프로그램 종료
 	
-	long add(long a, long b) {
-		long result = a+b;
-		return result;
-//		return a+b; //위의 두줄을 이와 같이 한 줄로 간단히 할 수 있다.
-	}
-	// 두 값을 받아서 둘중에 큰 값을 반환하는 메서드를 작성하시오.
-	long max(long a, long b) {
-		if(a>b)
-			//This method must return a result of type long
-			return a; // 조건식이 참일떄만 실행
-		else
-			return b; // 조건식이 거짓일때 실행
-	}
-	long subtract(long a, long b) { return a-b;}
-	long multiply(long a, long b) {return a*b;}
-	double divide(double a, double b) {
-		return a / b;
-	}
+	static void change(int x) { //5. change 메서드 실행
+		x = 1000; // 지역변수 x변수에 1000대입
+		System.out.println("change() : x = " + x); //6. println 메서드 실행 In, 7. 콘솔 출력시 out
+	} //메서드 종료, 지역변수 ㅃㅇ
 }
