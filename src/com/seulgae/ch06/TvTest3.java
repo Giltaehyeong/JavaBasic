df7c96cc430142363d5630a1e13f08169c146268
seulgae <r1605866@naver.com>
2022-05-31 17:24:27 +0900

    Ch06. 객체지향 프로그래밍(1) / TvTest4.java * 실습목표 - 참조변수 배열을 선언하여 각 배열의 요소에 객체 생성과 값을 저장하고 메서드를 호출 후 결과값 출력하기.

--- src/com/seulgae/ch06/TvTest3.java
+++ src/com/seulgae/ch06/TvTest3.java
@@ -5,7 +5,7 @@ class Tv3{
 	String color; //색상
 	boolean power; //전원상태
 	int channel; // 채널
-	
+
 	// Tv의 기능(메서드)
 	void power() { power = !power; } // Tv를 켜거나 끄는 기능을 하는 메서드
 	void channelUp() { ++channel; } // Tv의 채널을 높이는 기능을 하는 메서드
@@ -18,11 +18,11 @@ public class TvTest3 {
 		Tv t2 = new Tv();
 		System.out.println("t1의 channel값은 " + t1.channel + "입니다.");
 		System.out.println("t2의 channel값은 " + t2.channel + "입니다.");
-		
+
 		t2 = t1; // t1이 저장하고 있는 값(주소)을 t2에 저장한다.
 		t1.channel = 7; // channel 값을 7로 한다.
 		System.out.println("t1의 channel값을 7로 변경하였습니다.");
-		
+
 		System.out.println("t1의 channel값은 " + t1.channel + "입니다.");
 		System.out.println("t2의 channel값은 " + t2.channel + "입니다.");
 	}
