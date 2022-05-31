5c4d416d8b39521643b4b8fd43bf5ea6e7c3d1fe
seulgae <r1605866@naver.com>
2022-05-31 21:31:10 +0900

    Ch06. 객체지향 프로그래밍(1) / CardTest.java * 실습목표 - 클래스변수와 인스턴스변수의 차이를 이해하기 위한 프로그램 + 주석설명 추가(5.31)

--- src/com/seulgae/ch06/CardTest.java
+++ src/com/seulgae/ch06/CardTest.java
@@ -5,12 +5,14 @@ public class CardTest {
 	public static void main(String[] args) {
 		System.out.println("Card.width = " + Card.width);
 		System.out.println("Card.height = " + Card.height);
+		//클래스변수(static 변수)는 객체생성 없이 '클래스이름.클래스변수'로 직접 사용 가능하다.
 		
 		Card c1 = new Card();
 		c1.kind = "Heart";
 		c1.number = 7;
 		
 		Card c2 = new Card();
+		//인스턴스변수의 값을 변경한다.
 		c2.kind = "Spade";
 		c2.number = 4;
 		
@@ -19,6 +21,7 @@ public class CardTest {
 		System.out.println("c2은 " + c2.kind + ", " + c2.number
 				+ "이며, 크기는 (" + c2.width + ", " + c2.height + ")");
 		System.out.println("c1의 width와 height를 각각 50, 80으로 변경합니다.");
+		//클래스변수의 값을 변경한다.
 		c1.width = 50;
 		c1.height = 80;
 		
