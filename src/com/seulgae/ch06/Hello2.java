a3197773dccb4712b712863949347e8239d15899
seulgae <r1605866@naver.com>
2022-05-31 15:43:54 +0900

    5.31 Hello2.java * 실습목표 - 한 파일에 여러 클래스 작성하기

---
+++ src/com/seulgae/ch06/Hello2.java
@@ -0,0 +1,14 @@
+package com.seulgae.ch06;
+//하나의 소스파일에는 하나의 클래스만 작성하는 것이 바람직
+//public class Hello2 {} // 소스파일의 이름은 public class 이름과 일치, public class가 없는 경우에는 상관없음
+class hello2{}
+class Hello3 {
+	public static void main(String[] args) { // 소스파일명과 클래스 이름이 다를 경우 컴파일러가 알아서 아무거나 실행시킴.
+		System.out.println("hello");
+	}
+}
+/*
+public class Hello3 {} // 하나의 소스파일에는 하나의 public class만 허용
+//The public type Hello3 must be defined in its own file
+ */
+class Hello4 {}
