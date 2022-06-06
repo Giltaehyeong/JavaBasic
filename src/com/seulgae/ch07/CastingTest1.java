64df93e011a52e815d8c9d13abad68da52716ea1
seulgae <r1605866@naver.com>
2022-06-06 10:10:28 +0900

    06.06 참조변수의 형변환 실습 / CastingTest1.java

--- src/com/seulgae/ch07/CastingTest1.java
+++ src/com/seulgae/ch07/CastingTest1.java
@@ -6,9 +6,9 @@ class CastingTest1 {
 		FireEngine fe2 = null;
 
 		fe.water();
-		car = fe;    // car =(Car)fe;���� ����ȯ�� ������ ���´�.
+		car = fe;    // car =(Car)fe;에서 형변환이 생략된 형태다.
 //		car.water();	
-		fe2 = (FireEngine)car; // �ڼ�Ÿ�� �� ����Ÿ��
+		fe2 = (FireEngine)car; // 자손타입 <--- 조상타입
 		fe2.water();
 	}
 }
@@ -17,17 +17,17 @@ class Car {
 	String color;
 	int door;
 
-	void drive() { 		// �����ϴ� ���
+	void drive() { 		// 운전하는 기능
 		System.out.println("drive, Brrrr~");
 	}
 
-	void stop() {		// ���ߴ� ���	
+	void stop() {		// 멈추는 기능	
 		System.out.println("stop!!!");	
 	}
 }
 
-class FireEngine extends Car {	// �ҹ���
-	void water() {		// ���� �Ѹ��� ���
+class FireEngine extends Car {	// 소방차
+	void water() {		// 물을 뿌리는 기능
 		System.out.println("water!!!");
 	}
 }
